473720d4f6a040a453359714a3a5df242382e50d
nickogden91 <nick@kunasystems.com>
2018-06-27 19:22:38 -0700

    contained cost variable inside recursive function. Also differentiated time.
    crafting time: time to craft item from inventory crafting
    production time: time to craft item if item has to be crafted in building or
    assembly machine
    smelting time: time spent in electric/steel furnace

--- resource_calculator.py
+++ resource_calculator.py
@@ -6,8 +6,7 @@ class ResourceCalculator:
 
     def __init__(self):
         self.build_dict()
-        #self.test()
-        self.clear()
+        self.test()
 
     def build_dict(self):
         self.items = {}
@@ -21,33 +20,33 @@ class ResourceCalculator:
             else:
                 for i in data:
                     ingredients[i.split(': ')[0]] = float(i.split(': ')[1])
-            time = float(Config[item]['time'])
-            self.items[item] = {"ingredients": ingredients, "time": time}
+            self.items[item] = {"ingredients": ingredients, \
+                                "crafting_time": float(Config[item].get('crafting_time', 0)), \
+                                "production_time": float(Config[item].get('production_time', 0)), \
+                                "smelting_time": float(Config[item].get('smelting_time', 0)), \
+                                }
         #print(self.items)
 
-    def clear(self):
-        self.cost = defaultdict(int)
-        self.time = 0
-
-    def add(self, item_dict, max_recurse_depth):
+    def get_cost(self, item_dict, max_recurse_depth):
+        cost = defaultdict(int)
+        cost['crafting_time'] = 0
         for (name,count) in item_dict.items():
             item = self.items[name]
-            if max_recurse_depth > 0:
-                self.time += item['time'] * count
+            print(name, count, item['crafting_time'])
+            cost['crafting_time'] += item['crafting_time'] * count
+            cost['production_time'] += item['production_time'] * count
+            cost['smelting_time'] += item['smelting_time'] * count
             if item['ingredients'] == None or max_recurse_depth == 0:
-                self.cost[name] += count
+                cost[name] += count
             else:
-                self.add({key:value*count for (key,value) in item['ingredients'].items()}, max_recurse_depth-1)
-
-    def print_(self):
-        print("cost:", dict(self.cost))
-        print("time:", self.time)
+                subcost = self.get_cost({key:value*count for (key,value) in item['ingredients'].items()}, max_recurse_depth-1)
+                for (name,count) in subcost.items():
+                    cost[name] += count
+        return dict(cost)
 
     def test(self):
         for item in self.items:
-            self.clear()
-            self.add({item: 1})
-            #print(item, dict(self.cost))
+            print(self.get_cost({item: 1}, 1000))
 
 
 
@@ -55,5 +54,4 @@ if __name__ == '__main__':
     R = ResourceCalculator()
     #R.add({"science_pack_1": 1, "science_pack_2": 1, "science_pack_3": 1, "millitary_science_pack": 1, "production_science_pack": 1, "high_tech_science_pack": 1})
     #R.add({"rocket_silo": 1, "rocket_part": 100, "satellite": 1})
-    R.add({"advanced_circuit": 1}, 2)
-    R.print_()
+    print(R.get_cost({"advanced_circuit": 1}, 3))
