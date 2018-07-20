b0aa1095efe6129b9f2e3d09bde9a76f2b041d63
nickogden91 <nick@kunasystems.com>
2018-07-19 18:03:07 -0700

    show factory tree and raw materials

--- resource_calculator.py
+++ resource_calculator.py
@@ -1,11 +1,13 @@
 import os
 from collections import defaultdict
 from configparser import ConfigParser
+from math import ceil
 
 AM1_CRAFTING_SPEED = 0.5
 AM2_CRAFTING_SPEED = 0.75
 AM3_CRAFTING_SPEED = 1.25
-CRAFTING_SPEED = AM2_CRAFTING_SPEED
+AM3_CRAFTING_SPEED_SP3 = 1.25 * 3
+CRAFTING_SPEED = AM3_CRAFTING_SPEED
 
 TB_HALF_CAPACITY = 6.66666666
 TB_FULL_CAPACITY = 13.3333333
@@ -13,6 +15,7 @@ FTB_HALF_CAPACITY = 13.333333
 FTB_FULL_CAPACITY = 26.6666666
 ETB_HALF_CAPACITY = 20
 ETB_FULL_CAPACITY = 40
+PIPE_FULL_CAPACITY = 1000
 
 class ResourceCalculator:
 
@@ -36,6 +39,8 @@ class ResourceCalculator:
                                 "crafting_time": float(Config[item].get('crafting_time', 0)), \
                                 "production_time": float(Config[item].get('production_time', 0)), \
                                 "smelting_time": float(Config[item].get('smelting_time', 0)), \
+                                "mining_time": float(Config[item].get('mining_time', 0)), \
+                                "transport": Config[item].get('transport', 'transport_belt'), \
                                 }
         #print(self.items)
 
@@ -47,6 +52,7 @@ class ResourceCalculator:
         cost['smelting_time'] = 0
         for (name,count) in item_dict.items():
             item = self.items[name]
+            #print(1, name, item['crafting_time'] * count)
             cost['crafting_time'] += item['crafting_time'] * count
             cost['production_time'] += item['production_time'] * count
             cost['smelting_time'] += item['smelting_time'] * count
@@ -56,6 +62,7 @@ class ResourceCalculator:
                 subdict = {key:value*count for (key,value) in item['ingredients'].items()}
                 subcost = self.get_cost(subdict, max_recurse_depth-1)
                 if max_recurse_depth > 1:
+                    #print(2, name, subcost['crafting_time'])
                     cost['crafting_time'] += subcost['crafting_time']
                     cost['production_time'] += subcost['production_time']
                     cost['smelting_time'] += subcost['smelting_time']
@@ -70,26 +77,69 @@ class ResourceCalculator:
 
     def get_factory(self, item_name, rate):
         '''item_name: item to craft, rate: desired products/s'''
+        factory = {'item_name': item_name, 'rate': rate, 'ingredients': []}
         base_cost = self.get_cost({item_name: rate}, 1)
-        num_assembly_machines = base_cost['crafting_time'] / CRAFTING_SPEED
-        print('assembly machines:', num_assembly_machines)
+        production_time = base_cost['crafting_time']
+        if not production_time:
+            production_time = base_cost['production_time']
+        factory['assembly_machines'] = production_time / CRAFTING_SPEED
         for (ingredient, count) in base_cost['ingredients'].items():
-            print("%s: %s/s %s" % (ingredient, count, self.get_belt(count)))
+            factory['ingredients'].append((ingredient, count))
+        return factory
 
 
-    def get_belt(self, rate):
-        '''returns a belt type string given the item rate'''
-        if rate <= 13.3333:
-            return "Transport Belt (%d%%)" % (100*rate/TB_FULL_CAPACITY)
-        if rate <= 26.6666:
-            return "Fast Transport Belt (%d%%)" % (100*rate/FTB_FULL_CAPACITY)
-        if rate <= 40:
-            return "Express Transport Belt (%d%%)" % (100*rate/ETB_FULL_CAPACITY)
+    def print_factory(self, item_name, rate, recurse_level=0, raw_materials=defaultdict(int)):
+        if recurse_level == 0:
+            print("Factory:")
+        factory = self.get_factory(item_name, rate)
+        indent_str = '        '*recurse_level
+        print('\n%s%s: %4.2f/s' % (indent_str, item_name, rate))
+        print('%sassembly_machines: %d' % (indent_str, max(factory['assembly_machines'], 1)))
+        print('%s%s' % (indent_str, self.get_transport(item_name, rate)))
+        for item_n, count, in factory['ingredients']:
+            if self.items[item_n]['smelting_time'] or self.items[item_n]['ingredients'] is None and not 'ore' in item_n:
+                raw_materials[item_n] += count
+            if self.items[item_n]['smelting_time'] is None or self.items[item_n]['ingredients'] is not None:
+                self.print_factory(item_n, count, recurse_level=recurse_level+1, raw_materials=raw_materials)
+
+        if recurse_level == 0:
+            print('\nRaw Materials:\n')
+            for item in raw_materials:
+                if self.items[item]['ingredients'] and 'plate' in item:
+                    ore = list(self.items[item]['ingredients'].keys())[0]
+                    print('%s: %.2f, furnaces: %d, mining drills: %d' % \
+                        (item, \
+                         raw_materials[item], \
+                         ceil(raw_materials[item]*(self.items[item]['smelting_time'])), \
+                         ceil((raw_materials[item]*self.items[ore]['mining_time'])))\
+                        )
+                else:
+                    print('%s: %.2f' % (item, raw_materials[item]))
+
+
+    def get_production(self, item_name, rate):
+        '''returns a production machine string (or equivalent) given the item rate'''
+        pass
+
+
+    def get_transport(self, item_name, rate):
+        '''returns a belt type string (or equivalent) given the item rate'''
+        item = self.items[item_name]
+        if item['transport'] == 'transport_belt':
+            # if rate <= 13.3333:
+            #     return "transport belt         (%d%%)" % (100*rate/TB_FULL_CAPACITY)
+            # if rate <= 26.6666:
+            #     return "fast transport belt    (%d%%)" % (100*rate/FTB_FULL_CAPACITY)
+            # else:
+            #     return "express transport belt (%d%%)" % (100*rate/ETB_FULL_CAPACITY)
+            return "express transport belt (%d%%)" % (100*rate/ETB_FULL_CAPACITY)
+        if item['transport'] == 'pipe':
+            return "pipe                   (%d%%)" % (100*rate/PIPE_FULL_CAPACITY)
 
 
 if __name__ == '__main__':
     R = ResourceCalculator()
     #R.add({"science_pack_1": 1, "science_pack_2": 1, "science_pack_3": 1, "millitary_science_pack": 1, "production_science_pack": 1, "high_tech_science_pack": 1})
     #R.add({"rocket_silo": 1, "rocket_part": 100, "satellite": 1})
-    #print(R.get_cost({"advanced_circuit": 1}, 1))
-    R.get_factory("advanced_circuit", 10)
+    R.print_factory("advanced_circuit", 10)
+    #R.get_factory("express_transport_belt", 1)
